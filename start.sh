b3a8d6f9555f17e2d7ca09fb746441e198fed2c0
root <root@base.us-south1-b.c.high-availability-456718.internal>
2025-04-16 18:50:48 +0000

    Inter commit. switching branch

--- start.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-sudo docker service ls 2> /dev/null
-declare -i ret=$?
-if [[ ! $ret -eq 0 ]]; then
-	sudo docker swarm init 2>&1 > /dev/null
-	sudo docker stack deploy -c docker-compose.yml wordpress-stack
-fi
