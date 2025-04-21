bfdcd41d37bffb8de7153d6820d105d759a21e7f
root <root@base.us-south1-b.c.high-availability-456718.internal>
2025-04-21 01:55:26 +0000

    Install script

---
+++ install.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+cp docker-compose.yml restart.sh -t /opt
--- restart.sh
+++ restart.sh
@@ -2,9 +2,5 @@
 
 declare -i lines=$(docker compose ls | awk '$1=="high-availability-wordpress" {print}' | wc -l)
 if [[ $lines -eq 1 ]]; then
-	sudo docker compose -f /home/tylermg640/final-project/high-availability-wordpress/docker-compose.yml up -d
-fi
-lines=$(docker compose ls | awk '$1=="final-project" {print}' | wc -l)
-if [[ $lines -eq 1 ]]; then
-	sudo docker compose -f /home/tylermg640/final-project/docker-compose.yml up -d
+	sudo docker compose -f /opt/docker-compose.yml up -d
 fi
