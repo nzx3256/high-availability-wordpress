8c9c6196e6cb5d4e818b12b232ac07247c0af0dd
root <root@base.us-south1-b.c.high-availability-456718.internal>
2025-04-21 01:13:11 +0000

    Cron

---
+++ restart.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+declare -i lines=$(docker compose ls | awk '$1=="high-availability-wordpress" {print}' | wc -l)
+if [[ $lines -eq 1 ]]; then
+	sudo docker compose -f /home/tylermg640/final-project/high-availability-wordpress/docker-compose.yml up -d
+fi
