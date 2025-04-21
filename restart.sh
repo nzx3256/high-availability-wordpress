3561c980bc4618d488b24780b5ac80a5a449937b
root <root@base.us-south1-b.c.high-availability-456718.internal>
2025-04-21 01:46:28 +0000

    Cron fix and Feature

--- restart.sh
+++ restart.sh
@@ -4,3 +4,7 @@ declare -i lines=$(docker compose ls | awk '$1=="high-availability-wordpress" {p
 if [[ $lines -eq 1 ]]; then
 	sudo docker compose -f /home/tylermg640/final-project/high-availability-wordpress/docker-compose.yml up -d
 fi
+lines=$(docker compose ls | awk '$1=="final-project" {print}' | wc -l)
+if [[ $lines -eq 1 ]]; then
+	sudo docker compose -f /home/tylermg640/final-project/docker-compose.yml up -d
+fi
